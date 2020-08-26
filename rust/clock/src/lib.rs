c0978f7856400594d99db9da0242a2327a5e6c2e
Leonardo Razovic <leorazzo@gmail.com>
2020-08-26 12:29:50 +0200

    rust: clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,24 @@
-pub struct Clock;
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    time: i32,
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{:02}:{:02}", self.time / 60, self.time % 60)
+    }
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock {
+            time: (60 * hours + minutes).rem_euclid(24 * 60),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(0, self.time + minutes)
     }
 }
