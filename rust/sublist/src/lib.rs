c139b6693f859ff3fcfd5b0c679183b0d7ff3e8b
Leonardo Razovic <leorazzo@gmail.com>
2020-08-28 22:03:16 +0200

    rust: second iteration using match guard

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -12,19 +12,12 @@ fn contains<T: PartialEq>(list_one: &[T], list_two: &[T]) -> bool {
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
     match (_first_list.is_empty(), _second_list.is_empty()) {
-        (true, true) => return Comparison::Equal,
-        (false, true) => return Comparison::Superlist,
-        (true, false) => return Comparison::Sublist,
-        (false, false) => {
-            if _first_list.eq(_second_list) {
-                Comparison::Equal
-            } else if contains(_first_list, _second_list) {
-                Comparison::Sublist
-            } else if contains(_second_list, _first_list) {
-                Comparison::Superlist
-            } else {
-                Comparison::Unequal
-            }
-        }
+        (true, true) => Comparison::Equal,
+        (false, true) => Comparison::Superlist,
+        (true, false) => Comparison::Sublist,
+        (false, false) if _first_list.eq(_second_list) => Comparison::Equal,
+        (false, false) if contains(_first_list, _second_list) => Comparison::Sublist,
+        (false, false) if contains(_second_list, _first_list) => Comparison::Superlist,
+        (false, false) => Comparison::Unequal,
     }
 }
