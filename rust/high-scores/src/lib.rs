9a9d33db2be63c475408ca0c8eb9b6c3712cca3e
Leonardo Razovic <leorazzo@gmail.com>
2020-08-30 21:44:08 +0200

    rust: high scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct HighScores {
+    scores: Vec<u32>,
+}
+
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        Self {
+            scores: scores.to_vec(),
+        }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        &self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut rank = self.scores.to_vec();
+        rank.sort_unstable();
+        rank.reverse();
+        rank.truncate(3);
+        rank
+    }
+}
